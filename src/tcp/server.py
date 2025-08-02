3581fab53760cbe9fae54c9357bb26648aa2c7b8
leha-pasechnikov <pasechnikov.leha@inbox.ru>
2025-08-02 16:50:41 +0000

    feat(server): created TCP server

---
+++ src/tcp/server.py
@@ -0,0 +1,39 @@
+import socket
+import os
+import sys
+
+HOST = (socket.gethostname(), 1000)
+filename = "file_server.txt"
+max_turn = 10
+chunk = 1024
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    server.bind(HOST)
+except socket.error as err:
+    sys.exit(1)
+
+server.listen(max_turn)
+print("Server starting...")
+
+
+while True:
+    conn, addr = server.accept()
+
+    if os.path.exists(filename):
+        with open(filename, "rb") as file:
+            print(f"Connected - {addr[0]}:{addr[1]}")
+
+            data = file.read(chunk)
+            while data:
+                conn.sendall(data)
+                data = file.read(chunk)
+
+            conn.close()
+    else:
+        print(f"Ошибка, {filename} не существует")
+        sys.exit(1)
+
+
+
